b72a5d9a28209eccafbea39e9d8bbe18522964c6
Anna Tochk <anitochka1105@gmail.com>
2025-01-06 18:50:43 +0500

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+	str = String(str); // Преобразуем основную строку в строку
+	const repeatTimes = options.repeatTimes || 1; // Количество повторений строки
+	const separator = options.separator || '+'; // Разделитель между повторениями
+	const addition = options.addition !== undefined ? String(options.addition) : ''; // Добавление
+	const additionRepeatTimes = options.additionRepeatTimes || 1; // Количество повторений добавления
+	const additionSeparator = options.additionSeparator || '|'; // Разделитель добавлений
+
+	// Собираем часть с добавлением
+	const additionPart = Array(additionRepeatTimes)
+		.fill(addition)
+		.join(additionSeparator);
+
+	// Собираем всю строку
+	const result = Array(repeatTimes)
+		.fill(str + additionPart)
+		.join(separator);
+
+	return result;
 }
 
 module.exports = {
-  repeater
+	repeater
 };
